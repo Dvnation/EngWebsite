92d97c067cb5cca7a6b2c7e89cf9944fe2090118
Dvnation <dvnugo@gmail.com>
2024-10-01 21:07:47 +0100

    new

---
+++ js/edit.js
@@ -0,0 +1,13 @@
+
+
+const blog = ()=>{
+   document.querySelector('#blog').scrollIntoView({behavior:"smooth"})
+}
+const about = ()=>{
+    document.querySelector('#about-us').scrollIntoView({behavior:"smooth"})
+
+}
+
+const contact =()=>{
+    
+}
